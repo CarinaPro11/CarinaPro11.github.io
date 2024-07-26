eb17fcb277d468ba23fc0596e2de108227a233d4
ShadowXPA <51165769+ShadowXPA@users.noreply.github.com>
2024-07-26 11:58:02 -0400

    first commit

---
+++ js/data.js
@@ -0,0 +1,94 @@
+const timeline = [
+    {
+        icon: 'mortarboard',
+        name: '12º ano',
+        date: {
+            start: {
+                year: 2017,
+                month: 10,
+                day: 15
+            },
+            end: {
+                year: 2020,
+                month: 7,
+                day: 24
+            }
+        },
+        description: 'Curso de Educação e Formação de Adultos (EFA)',
+        links: [
+            {
+                uri: 'https://www.aecoimbracentro.pt/agrupamento/escola/1/jaime-cortesao',
+                icon: 'link-45deg'
+            }
+        ]
+    },
+    {
+        icon: 'mortarboard',
+        name: 'Ensino Superior',
+        date: {
+            start: {
+                year: 2021,
+                month: 10,
+                day: 15
+            },
+            end: {
+                year: 2023,
+                month: 7,
+                day: 18
+            }
+        },
+        description: 'Curso Técnico Superior Profissional - Maneio de Equinos, Equitação Terapêutica e de Lazer',
+        links: [
+            {
+                uri: 'https://www.esac.pt',
+                icon: 'link-45deg'
+            }
+        ]
+    },
+    {
+        icon: 'briefcase',
+        name: 'Centro Hípico de Coimbra<br/>- Estágio',
+        date: {
+            start: {
+                year: 2023,
+                month: 2,
+                day: 27
+            },
+            end: {
+                year: 2023,
+                month: 5,
+                day: 27
+            }
+        },
+        description: '- Manutenção das boxes dos equinos da escola de equitação<br/>- Trabalho de cavalos à guia com ou sem rédeas<br/>- Aparelhar e desaparelhar cavalos<br/>- Guia de passeios para clientes a cavalo/pónei',
+        links: [
+            {
+                uri: 'https://www.instagram.com/centro_hipico_coimbra',
+                icon: 'instagram'
+            }
+        ]
+    },
+    {
+        icon: 'briefcase',
+        name: 'Indústria e Comércio Alimentar<br/>- Part-time',
+        date: {
+            start: {
+                year: 2023,
+                month: 9,
+                day: 15
+            },
+            end: {
+                year: 2024,
+                month: 7,
+                day: 31
+            }
+        },
+        description: 'Empregada de Refeitório<br/>- Preparar o espaço para a entrada das crianças<br/>- Servir à mesa<br/>- Supervisionar as crianças enquanto almoçam<br/>- Limpeza do estabelecimento',
+        links: [
+            {
+                uri: 'https://www.linkedin.com/company/ica-ind%C3%BAstria-e-com%C3%A9rcio-alimentar-s-a',
+                icon: 'linkedin'
+            }
+        ]
+    }
+]
---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(() => {
+    const selectNextBackground = () => {
+        const elements = $('.background')
+        const count = elements.length
+        const nextIndex = Math.floor(Math.random() * count)
+        const nextElement = $(elements[nextIndex])
+
+        if (nextElement.hasClass('active')) {
+            return selectNextBackground()
+        }
+
+        const x = Math.floor(Math.random() * 100)
+        const y = Math.floor(Math.random() * 100)
+        nextElement.css('background-position', `${x}% ${y}%`)
+        nextElement.addClass('active')
+        setTimeout(() => {
+            nextElement.removeClass('active')
+        }, Math.floor(Math.random() * 3500) + 3500)
+        return nextIndex
+    }
+
+    $('.background').each((index, element) => {
+        const elem = $(element)
+        const background = elem.attr('data-bg')
+
+        elem.css('background-image', `url('img/${background}')`)
+    })
+
+    setInterval(() => {
+        selectNextBackground()
+    }, 3500)
+})
