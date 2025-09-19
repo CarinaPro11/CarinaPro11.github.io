e3502502d5142526749d9573fd8effce24582627
Pedro Alves <51165769+ShadowXPA@users.noreply.github.com>
2025-09-19 12:29:36 -0400

    update to v3.0.0

---
+++ _next/static/chunks/a5f4695e4d39fbb0.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/9b2f15e96aec358c.js",
+  "static/chunks/turbopack-0274536c4170ea6e.js"
+])
---
+++ _next/static/chunks/e60798c2eebbf1ee.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/9b2f15e96aec358c.js",
+  "static/chunks/turbopack-0013d7bea3a18168.js"
+])
